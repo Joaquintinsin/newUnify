a05bb005bd5ac3bb1ff6c61eb03e9f893c3bf379
Joaquin Tissera <phoenix_css@hotmail.com>
2024-09-01 00:10:39 -0300

    Database changes
    Unique constraints to users

--- db/migrate/202423060138_create_users.rb
+++ db/migrate/202423060138_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, unique: true
       t.string :name
       t.string :lastname
       t.string :cellphone
-      t.string :email
+      t.string :email, unique: true
       t.string :password
 
       t.timestamps
